ebdcdfab4a84d74aad4c3225f00a5c283658be7a
PLUSAlo <alo.marvel1@gmail.com>
2018-04-24 17:07:17 -0500

    Add files via upload

---
+++ src/main/java/com/questdot/firebasedatabaseexample/Data.java
@@ -0,0 +1,64 @@
+package com.questdot.firebasedatabaseexample;
+
+public class Data {
+
+    String key;
+
+    String name;
+    String email;
+    int phone;
+
+    public Data(){
+
+    }
+
+
+    public Data(String key, String name, String email, int phone) {
+        this.key = key;
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getPhone() {
+        return phone;
+    }
+
+    public void setPhone(int phone) {
+        this.phone = phone;
+    }
+
+    @Override
+    public String toString() {
+        return "Data{" +
+                "key='" + key + '\'' +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", phone=" + phone +
+                '}';
+    }
+}
